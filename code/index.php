23860e10618c9018e1983be2e1e2b4afe881104d
Umesh Ghimire <me.umeshuser@gmail.com>
2022-07-28 15:58:05 +0545

    update .gitignore

--- code/index.php
+++ code/index.php
@@ -10,11 +10,10 @@
 /**
  * Prints signup_header via wp_head.
  *
- * @param int $test_var Sample Variable.
+ * @param int    $test_var Sample Variable.
  * @param string $required_var any type of variable.
  * @return string string return garcha.
  */
-function test_function_for_php_cs($test_var = 1, $required_var)
-{
-    return 'Hello World' . $test_var . $required_var;
+function test_function_for_php_cs( $test_var = 1, $required_var ) {
+	return 'Hello World' . $test_var . $required_var;
 }
